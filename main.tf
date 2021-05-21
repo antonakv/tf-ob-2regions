6a887c357c3a2ed4f73f65dccc561533df16a5d3
Anton Akulov <antonakv@users.noreply.github.com>
2021-05-21 11:22:55 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  alias  = "aws-frankfurt"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "dynamodb-table-1" {
+  name           = "UserTable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "UserId"
+  range_key      = "UserTitle"
+
+  provider = aws.aws-frankfurt
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserTitle"
+    type = "S"
+  }
+
+  tags = {
+    Name = "dynamodb-table-user"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-table-2" {
+  name           = "UserTable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "UserId"
+  range_key      = "UserTitle"
+
+  provider = aws
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserTitle"
+    type = "S"
+  }
+
+  tags = {
+    Name = "dynamodb-table-user"
+  }
+}
